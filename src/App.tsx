05b672ffb1613d549cbff14c595cf67e52e337cf
Tyler Tierney <tytierney@yahoo.com>
2024-02-27 15:27:03 -0500

    Update game logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,37 @@ import "./index.css";
 
 export type TileValue = number | "mine";
 
+export interface ITile {
+  value: TileValue;
+  covered: boolean;
+  flagged: boolean;
+  background: "#c6c6c6" | "red";
+}
+
 class Board {
-  squares: Array<TileValue> = [];
+  squares: Array<ITile> = [];
   width: number = 9;
   height: number = 9;
   constructor(width = 9, height = 9) {
     this.width = width;
     this.height = height;
-    this.squares = Array(width * height).fill(0);
+    this.squares = Array(width * height)
+      .fill(null)
+      .map(() => ({
+        value: 0,
+        covered: true,
+        flagged: false,
+        background: "#c6c6c6",
+      }));
 
     let bombsToPlace = 10;
     while (bombsToPlace) {
-      const random = ~~(Math.random() * 81);
+      const random = ~~(Math.random() * (this.width * this.height));
       let i = 0;
-      while (i < 81) {
-        const curr = (random + i) % 81;
-        if (this.squares[curr] !== "mine") {
-          this.squares[curr] = "mine";
+      while (i < this.width * this.height) {
+        const curr = (random + i) % (this.width * this.height);
+        if (this.squares[curr].value !== "mine") {
+          this.squares[curr].value = "mine";
           bombsToPlace--;
           break;
         } else {
@@ -30,14 +44,29 @@ class Board {
     }
 
     this.squares = this.squares.map((square, i) => {
-      if (square !== "mine") {
-        return this.getProximity(i);
+      if (square.value !== "mine") {
+        return { ...square, value: this.getProximity(i) };
       }
       return square;
     });
   }
 
   getProximity = (index: number): number => {
+    const neighbors = this.getNeighbors(index);
+
+    const mineCount = neighbors
+      .map((i) => this.squares[i])
+      .reduce((count: number, neighbor: ITile) => {
+        if (neighbor.value === "mine") {
+          return count + 1;
+        }
+        return count;
+      }, 0);
+
+    return mineCount;
+  };
+
+  getNeighbors = (index: number) => {
     const row = Math.floor(index / this.width);
     const col = index % this.width;
 
@@ -57,42 +86,105 @@ class Board {
         }
       }
     }
+    return neighbors;
+  };
 
-    const mineCount = neighbors
-      .map((i) => this.squares[i])
-      .reduce((count: number, neighbor: TileValue) => {
-        if (neighbor === "mine") {
-          return count + 1;
-        }
-        return count;
-      }, 0);
+  findAdjacentTilesRecursively = (
+    index: number,
+    visited: Set<number>,
+    res: Set<number>
+  ) => {
+    const tiles = this.squares;
+    if (visited.has(index)) return res;
+    if (tiles[index].value !== 0 && tiles[index].value !== "mine") {
+      res.add(index);
+      return res;
+    }
+    res.add(index);
+    visited.add(index);
+    for (const neighbor of this.getNeighbors(index)) {
+      this.findAdjacentTilesRecursively(neighbor, visited, res);
+    }
 
-    return mineCount;
+    return res;
+  };
+
+  uncoverTile = (index: number) => {
+    const tile = this.squares[index];
+    tile.covered = false;
+    if (tile.value === "mine") {
+      console.log(`YOU HIT A MINE AT ${index}`);
+      tile.background = "red";
+      this.squares = this.squares.map((tile) => {
+        if (tile.value === "mine") return { ...tile, covered: false };
+        return tile;
+      });
+      return this.squares;
+    }
+    const adjacentTiles = this.findAdjacentTilesRecursively(
+      index,
+      new Set(),
+      new Set()
+    );
+
+    this.squares = this.squares.map((tile, i) => {
+      if (i === index) return { ...tile, covered: false };
+      if (adjacentTiles.has(i)) return { ...tile, covered: false };
+      return tile;
+    });
+
+    return this.squares;
   };
 }
 
 export default function App() {
-  const [width, setWidth] = useState(9);
-  const [height, setHeight] = useState(9);
+  // const [width, setWidth] = useState(7);
+  // const [height, setHeight] = useState(7);
+  const width = 7;
+  const height = 7;
   const [board, setBoard] = useState(new Board(width, height));
+  const [gameActive, setGameActive] = useState(true);
+
+  const uncoverTile = (index: number) => {
+    if (!gameActive) return;
+    setBoard((board) => {
+      return { ...board, squares: board.uncoverTile(index) };
+    });
+    if (board.squares[index].value === "mine") {
+      setGameActive(false);
+      return;
+    }
+  };
+
+  const resetGame = () => {
+    setBoard(() => new Board(width, height));
+    setGameActive(true);
+  };
 
   return (
-    <div>
-      {new Array(height).fill(null).map((_, i) => {
-        return (
-          <div className="row" key={i}>
-            {new Array(width).fill(null).map((_, j) => {
-              return (
-                <Tile
-                  key={i * width + j}
-                  value={board.squares[i * height + j]}
-                  index={i * width + j}
-                />
-              );
-            })}
-          </div>
-        );
-      })}
+    <div className="boardContainer">
+      <div className="boardHeader">
+        <button onClick={() => resetGame()}>reset</button>
+      </div>
+      <div className="board">
+        {new Array(height).fill(null).map((_, i) => {
+          return (
+            <div className="row" key={i}>
+              {new Array(width).fill(null).map((_, j) => {
+                const index = i * width + j;
+                return (
+                  <Tile
+                    key={index}
+                    tile={board.squares[i * height + j]}
+                    index={index}
+                    uncoverTile={uncoverTile}
+                  />
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,16 +1,61 @@
 import "./Tile.css";
-import { TileValue } from "../App";
+import { ITile, TileValue } from "../../App";
+
+const getColorFromValue = (value: TileValue): string => {
+  if (value === 1) return "blue";
+
+  if (value === 2) return "green";
+
+  if (value === 3) return "red";
+
+  if (value === 4) return "purple";
+
+  if (value === 5) return "maroon";
+
+  if (value === 6) return "lightblue";
+
+  if (value === 7) return "black";
+
+  if (value === 8) return "darkgray";
+  return "";
+};
+
+const getSymbolFromValue = (value: TileValue) => {
+  if (value === "mine") return "💣";
+  return value;
+};
 
 interface TileProps {
-  value: TileValue;
+  tile: ITile;
   index: number;
+  uncoverTile: (index: number) => void;
 }
 
-const Tile = ({ value, index }: TileProps) => {
+const Tile = ({ tile, index, uncoverTile }: TileProps) => {
+  const color = getColorFromValue(tile.value);
+
+  const symbol = getSymbolFromValue(tile.value);
+
   return (
-    <div className="tile">
-      {value}
-      <span style={{ position: "absolute" }}>{index}</span>
+    <div className="tile" style={{ backgroundColor: tile.background }}>
+      {tile.value !== 0 && (
+        <span className={`value`} style={{ color }}>
+          {symbol}
+        </span>
+      )}
+      {tile.covered === true && (
+        <div className="cover" onClick={() => uncoverTile(index)}></div>
+      )}
+      <span
+        style={{
+          position: "absolute",
+          bottom: 0,
+          right: 0,
+          fontSize: "0.5rem",
+        }}
+      >
+        {index}
+      </span>
     </div>
   );
 };
